6b756fe944702d79731e693ff247e26f45ea5d15
MFolgado <marcelofolgado@gmail.com>
2014-12-18 14:36:58 -0300

    busca funcionário form total mes

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -10,7 +10,9 @@ class HomesController < ApplicationController
   def show
     @orders = Order.where(date: [Date.today.beginning_of_month..Date.today.end_of_month])
     @date = params[:month] ? Date.parse(params[:month]) : Date.today
-    @employees = Employee.all
-    @employees = Employee.paginate(:page => params[:page], :per_page => 7)
+    results = Employee.where("name LIKE '%#{ params[:search]}%'")
+    @employees = results.paginate(:page => params[:page], :per_page => 7)
   end
+
+  
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,6 +5,14 @@ class Employee < ActiveRecord::Base
   validates_presence_of :name, message:"deve ser preenchido"
   validates_uniqueness_of :name
 
+  scope :ordered, -> {order(name: :asc)}
+
+  def self.search(params)
+    results = ordered
+    results.where(name: params)     
+    results
+  end
+
   def total_pedidos_mes
     orders.pedidos_do_mes.size
   end
